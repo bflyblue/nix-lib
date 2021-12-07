cd5a31fd09539c23bfbc795de654b3b60a56aa52
Shaun Sharples <shaun.sharples@gmail.com>
2021-12-07 11:54:11 +0200

    documentation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nix-lib
+
+Some useful functions I've made for myself.
+
+## requirements
+
+Helper for mach-nix requirements. It looks for includes of the form
+"-r filename.txt" and recursively inlines them instead as mach-nix doesn't
+currently support this feature.
--- lib.nix
+++ lib.nix
@@ -1,16 +1,25 @@
 { pkgs, lib }:
 
 let lines = file: lib.splitString "\n" (builtins.readFile file);
+
+    # Very simple test to look for lines starting with -r
     isRequire = lib.hasPrefix "-r ";
+
+    # Very simple way to extract filename from a -r line
     requireFile = dir: file: "${dir}/${lib.removePrefix "-r " file}";
+
+    # Return list of lines from a requirements file, resursively including
+    # files included using -r
     requires = file:
-      let part = lib.partition isRequire (lines file);
+      let part = lib.partition isRequire (lines file); # right = -r lines, wrong = rest
       in
-        if builtins.length part.right == 0 then
-          part.wrong
-        else
-          part.wrong ++ builtins.concatMap (x: requires (requireFile (builtins.dirOf file) x)) part.right;
-    requirements = file: builtins.concatStringsSep "\n" (requires file);
+        part.wrong ++ (
+          if builtins.length part.right == 0
+          then []
+          else builtins.concatMap (x: requires (requireFile (builtins.dirOf file) x)) part.right
+        );
 in {
-  inherit requirements;
+  # Helper for mach-nix requirements. It looks for includes of the form "-r filename.txt" and recursively
+  # inlines them instead as mach-nix doesn't currently support this feature.
+  requirements = file: builtins.concatStringsSep "\n" (requires file);
 }
