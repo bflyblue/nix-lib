078a2d17a6c7b4bfb23289638d6435b201724bcc
Shaun Sharples <shaun.sharples@gmail.com>
2021-12-09 02:45:43 +0200

    add buildLambdaImage

--- lib.nix
+++ lib.nix
@@ -18,8 +18,42 @@ let lines = file: lib.splitString "\n" (builtins.readFile file);
           then []
           else builtins.concatMap (x: requires (requireFile (builtins.dirOf file) x)) part.right
         );
+
+    # AWS Lambda Base Image for Custom Runtime
+    awsLambdaProvided = pkgs.dockerTools.pullImage {
+      imageName = "amazon/aws-lambda-provided";
+      imageDigest = "sha256:1b8ab5631ed5bf9aebabf9a0c04ad757e364ed8695fc3746ec704f477c49a915";
+      sha256 = "0i7vgpfcv7k9psjjbx0nkidkj19wk2pc7jvvqlw7h9ybgqh1h0lr";
+      finalImageName = "amazon/aws-lambda-provided";
+      finalImageTag = "al2";
+    };
+
+    # LD_LIBRARY_PATH breaks nix binaries so this wrapper unsets it
+    unsetLdLibraryPath = name: program: pkgs.writeScript name ''
+      #!/bin/sh
+      unset LD_LIBRARY_PATH
+      exec ${program} "$@"
+    ''; 
+
 in {
   # Helper for mach-nix requirements. It looks for includes of the form "-r filename.txt" and recursively
   # inlines them instead as mach-nix doesn't currently support this feature.
   requirements = file: builtins.concatStringsSep "\n" (requires file);
+
+  # Build a custom AWS Lambda docker container.
+  buildLambdaImage = { name, contents, bootstrap }:
+    pkgs.dockerTools.buildImage {
+      inherit name contents;
+      fromImage = awsLambdaProvided;
+      runAsRoot = ''
+        ${pkgs.runtimeShell}
+        ln -s /usr/bin/sh /bin/sh
+        cp ${noLibPath "bootstrap" bootstrap} /var/runtime/bootstrap
+      '';
+      config = {
+        EntryPoint = [ "/lambda-entrypoint.sh" ];
+        WorkingDir = "/var/task";
+        Cmd = [ "handler" ];
+      };
+    };
 }
